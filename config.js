a3718bcdb1aaabcb0c554c3f2acbf679f828a6f8
han <94585225+nghan16oct@users.noreply.github.com>
2022-04-04 21:27:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    titleWeb: "gửi nth",
+    introTitle: 'nguyễn thanh hải à',
+    introDesc: `chào mừng hải đến với tình iu của dth`,
+    btnIntro: 'hihi',
+    title: 'huyền thích hải nhiều lúm:> hải thích huyền khum?',
+    desc: 'thoát ra là có gùi nhé:>',
+    btnYes: 'cóoooo',
+    btnNo: 'khum',
+    question: 'sao hải lại thích huỳn z:3',
+    btnReply: 'gửi cho dth',
+    reply: 'tại cậu mặt dày quá đók',
+    mess: 'mình biết mà:3 mình thích hải nhiều lắm',
+    messDesc: 'z thì chần chừ gì nữa mà nhắn tin cho dth ngay nàoo',
+    btnAccept: 'ổce',
+    messLink: 'https://www.messenger.com/t/100040379926635'
 }
